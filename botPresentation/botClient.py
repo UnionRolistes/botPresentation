fc1ef56422687ed561a5c8db260e62d0f944b6e8
Tonitch <minecraftonitch@gmail.com>
2020-06-24 20:40:42 +0200

    From Function to Class

--- botPresentation/botClient.py
+++ botPresentation/botClient.py
@@ -1,23 +1,20 @@
 import discord
 
-botClient = discord.Client()
-
-
-@botClient.event
-async def on_message(message):
-    if message.author == botClient.user:
-        return
-
-    if message.content.startswith('$pres'):
-        await message.channel.send(
-            '{0.author.mention} regarde dans tes messages privées pour te présenter! :D'.format(message)
-        )
-
-        await message.author.send(
-            'Voici le lien de la présentation :'
-        )
 
 class botClient(discord.Client):
 
     async def on_ready(self):
         print(f"Bot {self.user} is ready")
+
+    async def on_message(self, message):
+        if message.author == botClient.user:
+            return
+
+        if message.content.startswith('$pres'):
+            await message.channel.send(
+                '{0.author.mention} regarde dans tes messages privées pour te présenter! :D'.format(message)
+            )
+
+            await message.author.send(
+                'Voici le lien de la présentation :'
+            )
